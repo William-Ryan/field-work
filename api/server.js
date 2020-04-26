9d643e8791a3d21fcb4e851c1704140897a2a9cf
William <billy.ryan.rom@gmail.com>
2020-04-26 01:34:22 -0400

    Add routers to server.js

--- api/server.js
+++ api/server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const helmet = require('helmet')
 const cors = require('cors')
 
+const authRouter = require('../auth/auth-router.js')
+const itemRouter = require('../items/itemsRouter.js')
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
+server.use('/api/auth', authRouter);
+server.use('/api/projects', itemRouter)
 
 server.get("/", (req, res) => {
     res.status(200).json({ api: "up" });
